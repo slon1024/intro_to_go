54718b5d4edc6da46f5ef9a3481ec6871d9d8a76
Vladimir Alekseichenko <vova@vova.me>
2013-11-07 13:07:09 +0100

    [go] example defer

---
+++ defer.go
@@ -0,0 +1,24 @@
+package main
+import "fmt"
+
+func main() {
+	fmt.Println(c())
+}
+
+func a() {
+	i := 0
+	defer fmt.Println(i)
+	i++
+	return
+}
+
+func b() {
+	for i := 0; i < 4; i++ {
+		defer fmt.Print(i)
+	}
+}
+
+func c() (i int) {
+	defer func() { i += 10 }()
+	return 1
+}
