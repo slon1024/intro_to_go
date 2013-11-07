a55c3d7e0b62144635534663a71f4ecb7240efc5
Vladimir Alekseichenko <vova@vova.me>
2013-11-07 13:07:59 +0100

    [go] flag: for console program

---
+++ flag.go
@@ -0,0 +1,18 @@
+//http://talks.golang.org/2012/simple.slide
+package main
+import (
+    "flag"
+    "fmt"
+    "time"
+)
+
+var (
+    message = flag.String("message", "Hello!", "what to say")
+    delay   = flag.Duration("delay", 2*time.Second, "how long to wait")
+)
+
+func main() {
+    flag.Parse()
+    fmt.Println(*message)
+    time.Sleep(*delay)
+}
